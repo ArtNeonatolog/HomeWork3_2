9886f3d35fd670fa94ade90f81784982ecc97c90
Артем <marcus201@mail.ru>
2022-12-13 21:26:01 +0500

    начальный коммит

---
+++ src/main/java/me/artsafuanov/homework3_2/HomeWork32Application.java
@@ -0,0 +1,13 @@
+package me.artsafuanov.homework3_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork32Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/artsafuanov/homework3_2/HomeWork32ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.artsafuanov.homework3_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork32ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
